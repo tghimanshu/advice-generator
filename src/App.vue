7b261d5e609063e74eac41c38d9f8ff12f7f19d6
Himanshu Gohil <himanshugohil234@gmail.com>
2023-02-05 16:29:13 +0530

    added the UI as well as the basic compontent layout to the app

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<script>
+// Importing the Components
+import AdviceShuffle from "./components/AdviceShuffle.vue";
+
+export default {
+  // Compontents to be used in the Screen
+  components: {
+    AdviceShuffle,
+  },
+  // Data To Be Show on Screen
+  data() {
+    return {
+      id: 1,
+      advice:
+        "It is easy to sit up and take notice, what's difficult is getting up and taking action.",
+    };
+  },
+};
+</script>
+
+<template>
+  <div class="advice">
+    <div class="advice_id">Advice #{{ id }}</div>
+
+    <div class="advice_text">"{{ advice }}"</div>
+
+    <div class="advice_divider">
+      <span></span>
+      <span></span>
+    </div>
+
+    <AdviceShuffle />
+  </div>
+
+  <!-- <div class="attribution">
+    Challenge by
+    <a href="https://www.frontendmentor.io?ref=challenge" target="_blank"
+      >Frontend Mentor</a
+    >. Coded by <a href="#">Your Name Here</a>.
+  </div> -->
+</template>
---
+++ src/components/AdviceShuffle.vue
@@ -0,0 +1,6 @@
+<script></script>
+<template>
+  <button class="advice_refresh">
+    <img src="/images/icon-dice.svg" alt="Looks Good" class="advice_dice" />
+  </button>
+</template>
